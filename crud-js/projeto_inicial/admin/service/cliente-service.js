2dfb1699b501ce299a0c867f9fbc6830914e6be2
Rafael Rosa <rafael.f.rosa1@gmail.com>
2021-10-14 23:40:30 -0300

    fim aula crud em javascript

--- crud-js/projeto_inicial/admin/service/cliente-service.js
+++ crud-js/projeto_inicial/admin/service/cliente-service.js
@@ -1,7 +1,11 @@
 const listaClientes = () => {
   return fetch(`http://localhost:3000/profile`)
   .then(resposta => {
-    return resposta.json()
+    if(resposta.ok) {
+      return resposta.json()
+    }
+    throw new Error('Não foi possível listar os clientes')
+    
   })
   // como fazer sem o fetch
   // const promise = new Promise((resolve, reject) => {
@@ -20,6 +24,68 @@ const listaClientes = () => {
   // return promise
 }
 
+const criaCliente = (nome, email) => {
+  return fetch(`http://localhost:3000/profile`, {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({
+      nome: nome,
+      email: email
+    })
+  })
+  .then(resposta => {
+    if(resposta.ok) {
+      return resposta.body
+    }
+    throw new Error('Não foi possível criar um cliente')
+  })
+}
+
+const removeCliente = (id) => {
+  return fetch(`http://localhost:3000/profile/${id}`, {
+    method: 'DELETE'
+  }).then(resposta => {
+    if(!resposta.ok){
+      throw new Error('Não foi possível remover o cliente')
+    }
+  })
+}
+
+const detalhaCliente = (id) => {
+  return fetch(`http://localhost:3000/profile/${id}`)
+  .then(resposta => {
+    if(resposta.ok){
+      return resposta.json()
+    }
+    throw new Error('Não foi possível detalhar o cliente')
+  })
+}
+
+const atualizaCliente = (id, nome, email) => {
+  return fetch(`http://localhost:3000/profile/${id}`, {
+    method: 'PUT', 
+    headers: {
+      'Content-Type' : 'application/json'
+    },
+    body: JSON.stringify({
+      nome: nome,
+      email: email
+    })
+  })
+  .then(resposta => {
+    if(resposta.ok){
+      return resposta.json()
+    }
+    throw new Error('Não foi possível atualizar o cliente')
+  })
+}
+
 export const clienteService = {
-  listaClientes
+  listaClientes,
+  criaCliente,
+  removeCliente,
+  detalhaCliente,
+  atualizaCliente
 }
